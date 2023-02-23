2b5acc3b4cc3028280ee432dd21506aefdc2ec8b
Numb246 <113155975+hvuquang@users.noreply.github.com>
2023-02-23 19:52:30 +0700

    fix
    - bug
    - UI

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let saveEl = document.getElementById("save-el")
+let foulEl = document.getElementById("foul-el")
 let countEl1 = document.getElementById("count-el1")
 let countEl2 = document.getElementById("count-el2")
 let foulBtn = document.getElementById("foul-btn")
 let foulEl1 = document.getElementById("count-el1")
 let foulEl2 = document.getElementById("count-el2")
-let count = 0
 let count1 = 0
 let count2 = 0
 let foul1 = 0 
@@ -19,16 +19,13 @@ let foulIncre1 = document.getElementById("foul-incre-btn1")
 let foulIncre2 = document.getElementById("foul-incre-btn2")
 
 function increment(i,id) {
-    // count += 1
     if (id === 1) {
         if(point == false) {
-            foul1 = parseInt(foulEl1.textContent)
             foul1 += 1
             foulEl1.textContent = foul1
         } else {
-            count = parseInt(countEl1.textContent)
-            count += i
-            countEl1.textContent = count
+            count1 += i
+            countEl1.textContent = count1
         }
     }
     else if (id === 2) {
@@ -38,9 +35,9 @@ function increment(i,id) {
             foulEl2.textContent = foul2
         }
         else {
-            count = parseInt(countEl2.textContent)
-            count += i
-            countEl2.textContent = count
+            count2 = parseInt(countEl2.textContent)
+            count2 += i
+            countEl2.textContent = count2
         }
     }
     else {
@@ -49,13 +46,18 @@ function increment(i,id) {
 }
 
 function saveFunc() {
-    let count1 = countEl1.textContent
-    let count2 = countEl2.textContent
     let countStr = count1 + " - " + count2 + ", "
+    let foulStr = foul1 + " - " + foul2 + ", "
     saveEl.textContent += countStr
+    foulEl.textContent += foulStr
     countEl1.textContent = 0
     countEl2.textContent = 0
-    count = 0
+    foulEl1.textContent = 0
+    foulEl2.textContent = 0
+    count1 = 0
+    count2 = 0
+    foul1 = 0
+    foul2 = 0
     id = 0
 }
 
@@ -76,8 +78,8 @@ function showElFunc() {
 function foulFunc() {
     if(foulBtn.textContent === "POINTS") {
         foulBtn.textContent = "FOULS"
-        foul1 = foulEl1.textContent
-        foul2 = foulEl2.textContent
+        foul1 = parseInt(foulEl1.textContent)
+        foul2 = parseInt(foulEl2.textContent)
         countEl1.textContent = count1
         countEl2.textContent = count2
         point = true
@@ -87,8 +89,8 @@ function foulFunc() {
     }
     else if (foulBtn.textContent === "FOULS") {
         foulBtn.textContent = "POINTS"
-        count1 = countEl1.textContent
-        count2 = countEl2.textContent
+        count1 = parseInt(countEl1.textContent)
+        count2 = parseInt(countEl2.textContent)
         foulEl1.textContent = foul1
         foulEl2.textContent = foul2
         point = false
